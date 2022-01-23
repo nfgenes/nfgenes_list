94f4aea8d3ed12775baf55139a4bd7776f5118f0
joselvelez <joselvelez@protonmail.com>
2022-01-23 14:19:46 -0600

    setup hardhat project

---
+++ contracts/NFgenesList.sol
@@ -0,0 +1,15 @@
+/// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.11;
+
+/// @title NFgenes Genesis List - List of ~20k unique human genes
+/// @notice This contract stores the URI pointer for the list of genes stored on IPFS
+/// @author The team at the NFgenes Project https://github.com/orgs/nfgenes/people
+
+// import "hardhat/consol.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact nfgenes@protonmail.com
+contract NFgenesList is Ownable {
+
+}
